340dfe2c58278953eed5e3e4d6c2fc93df856c2c
phucthuhigh <phucgamingpro@gmail.com>
2023-11-23 19:30:26 +0700

    change api url

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,6 @@
 export const LOCAL_STORAGE_ACCESS_TOKEN_NAME = "unimuzic-user-access-token";
 export const LOCAL_STORAGE_CURRENT_MUSIC = "unimuzic-current-music";
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const BASE_URL =
+    process.env.NODE_ENV !== "production"
+        ? "http://localhost:5000/api/"
+        : "https://uni-muzic-api.vercel.app/api/";
